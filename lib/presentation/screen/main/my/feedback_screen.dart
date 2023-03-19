7737abf8dba22079ac21d2a2699e75471c64391f
Hong Sunghwi <tnfy10@gmail.com>
2023-03-19 14:09:55 +0900

    피드백 화면 앱바 추가

--- lib/presentation/screen/main/my/feedback_screen.dart
+++ lib/presentation/screen/main/my/feedback_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/app_strings.dart';
+import '../../../../core/widget/base_app_bar.dart';
+import '../../../../core/widget/base_layout.dart';
+
 class FeedbackScreen extends StatelessWidget {
   static String get routeName => 'feedbackScreen';
 
@@ -7,6 +11,12 @@ class FeedbackScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BaseLayout(
+      resizeToAvoidBottomInset: false,
+      appBar: const BaseAppBar(title: AppStrings.feedback),
+      child: Column(
+        children: [],
+      ),
+    );
   }
 }
