b3677adabafb858ef5f55a088d02317a801b9baf
aloe <aloenode@gmail.com>
2023-04-22 23:33:29 +0900

    북마크한 아티클 위젯

--- lib/presentation/screen/main/bookmark/components/folder_components/article_con.dart
+++ lib/presentation/screen/main/bookmark/components/folder_components/article_con.dart
@@ -7,8 +7,26 @@ class ArticleCon extends StatelessWidget {
   final ArticleDetail article;
 const ArticleCon({ Key? key ,required this.article}) : super(key: key);
 
+  final Size size =const Size(156, 208);
+  final Size imageSize = const Size(156, 156);
+  final Size iconSize = const Size(12, 12);
+  final BorderRadius imageBr = const BorderRadius.all(Radius.circular(8));
+  final locationAssetPath = 'assets/images/map/location_icon.png';
+
+  final TextStyle titleStyle = const TextStyle(fontSize: 15,color: Color(0xff222222));
+  final TextStyle locationStyle = const TextStyle(fontSize: 12,color: Color(0xff666666));
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Column(
+      children: [
+        ClipRRect(
+  borderRadius:imageBr,
+  child: Image.network('',width: imageSize.width,height: imageSize.height,)
+), Text(article.title!,style: titleStyle,),
+  Row(children: [Image.asset(locationAssetPath,width: iconSize.width,height: iconSize.height,),
+  Text('test',style: locationStyle,)],)
+       
+      ],
+    );
   }
 }
