0db8d3680f6cb74942209d3147ce7eb9310fd54d
Hong Sunghwi <tnfy10@gmail.com>
2023-04-26 21:36:05 +0900

    lint

--- lib/presentation/screen/main/my/new_bookstore_report_screen.dart
+++ lib/presentation/screen/main/my/new_bookstore_report_screen.dart
@@ -3,8 +3,6 @@ import 'package:bookand/domain/model/kakao/search_keyword_response.dart';
 import 'package:bookand/domain/usecase/bookstore_report_use_case.dart';
 import 'package:bookand/presentation/provider/new_bookstore_report_provider.dart';
 import 'package:bookand/presentation/screen/main/my/new_bookstore_report_success.dart';
-import 'package:easy_debounce/easy_debounce.dart';
-import 'package:easy_debounce/easy_throttle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
